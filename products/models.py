9903290af5b3efcdba180fb61b02dceb5a55e67b
wdelight34 <wdelight34@gmail.com>
2022-05-04 21:32:30 +0900

    Add: database change

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'menus'
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'categories'
+
+class Product(models.Model):
+    name     = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'products'
